706b429cbabaacb11e6b2784cda8d05c6d4f2fac
Anouk <anoukdunuwille@gmail.com>
2022-10-21 16:01:30 +0530

    Seperated RandomWords classes into seperate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 //WHY IS GIT NOT WORKING
 
@@ -15,54 +15,5 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(primaryColor: Colors.purple[900]),
       //calls interface for main page?
       home: RandomWords());
-  }
-}
-
-//main pade interface?
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-//main class which is extended by RandomWords interface?
-class RandomWordsState extends State<RandomWords>{
-
-  //variable which calls a list a WordPaid words
-  final _randomWordPairs = <WordPair>[];
-
-  //widget to build the listview
-  Widget _buildList(){
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemBuilder: (context, item){
-        //generating a divider between entries
-        if(item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        if(index >= _randomWordPairs.length){
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-
-        //calls widget to put the wordpairs in each row
-        return _buildRow(_randomWordPairs[index]);
-      }
-     
-    );
-  }
-
-  //widget to output a random word pair per list item row
-  Widget _buildRow(WordPair pair){
-
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle (fontSize: 18)));
-
-  }
-
-  //main widget in randomWords class. 
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text('WordPare Generator')),
-      body: _buildList(),
-    );
   }
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+//main pade interface?
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+//main class which is extended by RandomWords interface?
+class RandomWordsState extends State<RandomWords>{
+
+  //variable which calls a list a WordPaid words
+  final _randomWordPairs = <WordPair>[];
+
+  //variable to store saved wordpairs
+  final _savedWordPairs = Set<WordPair>();
+
+  //widget to build the listview
+  Widget _buildList(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemBuilder: (context, item){
+        //generating a divider between entries
+        if(item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if(index >= _randomWordPairs.length){
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        //calls widget to put the wordpairs in each row
+        return _buildRow(_randomWordPairs[index]);
+      }
+     
+    );
+  }
+
+  //widget to output a random word pair per list item row
+  Widget _buildRow(WordPair pair){
+
+    final alreadySaved = _savedWordPairs.contains(pair);
+
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle (fontSize: 18)),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border, color: alreadySaved ? Colors.red : null,),
+    );
+
+  }
+
+  //main widget in randomWords class. 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPare Generator')),
+      body: _buildList(),
+    );
+  }
+}
