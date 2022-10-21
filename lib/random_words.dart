2365b0fdbf0b9a281bf7f133db19667aa9e26859
Anouk <anoukdunuwille@gmail.com>
2022-10-21 16:58:47 +0530

    final commit

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -61,10 +61,49 @@ class RandomWordsState extends State<RandomWords>{
 
   }
 
+  //function to route to different page
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair){
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0))
+            );
+          });
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs'),
+            ),
+            body: ListView(children: divided),
+          );
+
+        }
+      )
+    );
+
+  }
+
   //main widget in randomWords class. 
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('WordPare Generator')),
+      appBar: AppBar(
+        title: Text('WordPare Generator'), 
+
+        //creating an icon to navigate to a seperate 'favourite' page
+        actions: <Widget>[
+          IconButton(
+            onPressed: _pushSaved, 
+            icon: Icon(Icons.list)
+            )
+        ],
+      ),
       body: _buildList(),
     );
   }
